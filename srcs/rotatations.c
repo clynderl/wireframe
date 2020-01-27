c5c1ad8f6b4f623176959a1accb536358f7db59a
Chataya Lynderly <clynderl@at-g2.21-school.ru>
2020-01-27 12:33:50 +0300

    rotations

---
+++ srcs/rotatations.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotatations.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clynderl <clynderl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 12:31:16 by clynderl          #+#    #+#             */
+/*   Updated: 2020/01/27 12:33:31 by clynderl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void		ft_rotatation_x(int *y, double *z, double a)
+{
+	int p_y;
+
+	p_y = *y;
+	*y = p_y * cos(a) + *z * sin(a);
+	*z = -p_y * sin(a) + *z * cos(a);
+}
+
+void		ft_rotatation_y(int *x, double *z, double b)
+{
+	int p_x;
+
+	p_x = *x;
+	*x = p_x * cos(b) + *z * sin(b);
+	*z = -p_x * sin(b) + *z * cos(b);
+}
+
+void		ft_rotatation_z(int *x, int *y, double g)
+{
+	int p_x;
+	int p_y;
+
+	p_x = *x;
+	p_y = *y;
+	*x = p_x * cos(g) - p_y * sin(g);
+	*y = p_x * sin(g) + p_y * cos(g);
+}
